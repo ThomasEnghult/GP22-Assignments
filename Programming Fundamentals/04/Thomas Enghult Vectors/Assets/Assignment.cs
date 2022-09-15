3e47f2ccf6284abb3feaff45e72b01ee7e8a8434
Thomas Enghult <thomas.enghult@gmail.com>
2022-09-15 16:03:33 +0200

    Completed assignment 04

---
+++ Programming Fundamentals/04/Thomas Enghult Vectors/Assets/Assignment.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Assignment : ProcessingLite.GP21
+{
+
+    Vector2 start, move, deltaV;
+    bool existingCircle = false;
+    public bool keepVelocity = false;
+    public float mouseGravity = 1;
+    public float drag = 0;
+    public float ballRadius = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Stroke(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Background(255);
+        if (Input.GetMouseButtonDown(0) && !existingCircle)
+        {
+            existingCircle = true;
+            //Save starting position
+            start = new Vector2(MouseX, MouseY);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            //Distance between circle and mouse
+            deltaV = new Vector2(MouseX, MouseY) - start;
+
+            if(move == Vector2.zero || !keepVelocity)
+            {
+                //Move circle to mouse
+                move = deltaV* mouseGravity;
+            }
+            else
+            {
+                //Add deltaV vector to existing moving direction
+                move = move + deltaV*Time.deltaTime* mouseGravity;
+            }
+
+        }
+
+        //Handles bounce on right side
+        if (start.x > Width - ballRadius)
+        {
+            start.x = Width - ballRadius;
+            move = new Vector2(-move.x, move.y);
+        }
+
+        //Handles bounce on left side
+        if (start.x < 0 + ballRadius)
+        {
+            start.x = 0 + ballRadius;
+            move = new Vector2(-move.x, move.y);
+        }
+
+        //Handles bounce on top side
+        if (start.y > Height - ballRadius)
+        {
+            start.y = Height - ballRadius;
+            move = new Vector2(move.x, -move.y);
+        }
+
+        //Handles bounce on bottom side
+        if (start.y < 0 + ballRadius)
+        {
+            start.y = 0 + ballRadius;
+            move = new Vector2(move.x, -move.y);
+        }
+
+
+        start += move * Time.deltaTime;
+
+        //Adds drag to the circle, slowing it down
+        move = move - (move * Time.deltaTime)*drag;
+        //Debug.Log(start);
+
+        if (existingCircle)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                // Draws line from cicle to mouse
+                Line(start.x, start.y, MouseX, MouseY);
+            }
+
+            //Draw circle
+            Circle(start.x, start.y, ballRadius*2);
+        }
+    }
+}
